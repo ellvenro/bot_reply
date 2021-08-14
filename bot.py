ec3d088b901295dcf873de0e78efcdc0448eb445
ellvenro <lidiya.ellie@yandex.ru>
2021-08-14 14:24:25 +0300

    Основная часть

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 import config
 
+import json
 import telebot
+from telebot.types import Message
 
 print("Бот запущен. Нажмите Ctrl+C для завершения")
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def msg_start(message):
-    pass
+    bot.send_message(message.chat.id, 'Начало')
 
-@bot.message_hanler(content_types=['text'])
+@bot.message_handler(content_types=['text'])
 def msg_text(message):
-    pass
-    
+    if message.chat.id != config.chat_id:
+        bot.forward_message(config.chat_id, message.chat.id, message.message_id)
+    else:
+        bot.send_message(message.reply_to_message.id, message.text)
+
 bot.polling()
 #if __name__ == '__main__':
 #	bot.infinity_polling()
