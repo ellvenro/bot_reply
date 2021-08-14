b44278149ce8cb19c96fa8f4cf6a2f66e48c03c7
ellvenro <lidiya.ellie@yandex.ru>
2021-08-14 18:48:28 +0300

    Создание рассылки

--- bot.py
+++ bot.py
@@ -4,13 +4,32 @@ import json
 import telebot
 from telebot.types import Message
 
+lst = []
+
 print("Бот запущен. Нажмите Ctrl+C для завершения")
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def msg_start(message):
-    bot.send_message(message.chat.id, 'Начало')
+    bot.send_message(message.chat.id, 'Напиши мне')
+    lst.append(message.chat.id)
+
+@bot.message_handler(commands=['delete'])
+def msg_delete(message):
+    for index, item in enumerate(lst):
+        if (item == message.chat.id):
+            lst.remove(index)
+            break
+
+@bot.message_handler(commands=['mailing'])
+def msg_mailing(message):
+    if message.chat.id == config.chat_id:
+        bot.register_next_step_handler(message, mailing)
+    
+def mailing(message):
+    for item in lst:
+        bot.send_message(item, message.text)
 
 @bot.message_handler(content_types=['text'])
 def msg_text(message):
