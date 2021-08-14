e26be8168a7a8008c955d8930b5ee29445da3cd4
ellvenro <lidiya.ellie@yandex.ru>
2021-08-14 20:01:27 +0300

    Исправление ошибок

--- bot.py
+++ bot.py
@@ -6,27 +6,33 @@ from telebot.types import Message
 
 lst = []
 
+msg = {
+    'start' : 'Вы подписаны на рыссылку, также у Вас есть возможность общаться с администраторами, для этого просто отправьте мне сообщение, оно будет доставлено по адресу',
+    'mailing' : 'Следующее Ваше сообщение будет отправлено всем пользователям, использующим бота'
+}
+
 print("Бот запущен. Нажмите Ctrl+C для завершения")
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def msg_start(message):
-    bot.send_message(message.chat.id, 'Напиши мне')
+    bot.send_message(message.chat.id, msg['start'])
     lst.append(message.chat.id)
 
 @bot.message_handler(commands=['delete'])
 def msg_delete(message):
-    for index, item in enumerate(lst):
+    for item in lst:
         if (item == message.chat.id):
-            lst.remove(index)
+            lst.remove(item)
             break
 
 @bot.message_handler(commands=['mailing'])
 def msg_mailing(message):
     if message.chat.id == config.chat_id:
+        bot.send_message(message.chat.id, msg['mailing'])
         bot.register_next_step_handler(message, mailing)
-    
+
 def mailing(message):
     for item in lst:
         bot.send_message(item, message.text)
